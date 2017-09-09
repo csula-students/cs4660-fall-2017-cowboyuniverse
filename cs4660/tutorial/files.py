2424085642d1e960052741a28ab499ef940e55f3
cowboyuniverse <cowboyuniverse@gmail.com>
2017-09-09 16:06:01 -0700

    updating files, didn't realize this was part of our hw due today

--- cs4660/tutorial/files.py
+++ cs4660/tutorial/files.py
@@ -1,5 +1,7 @@
 """Files tests simple file read related operations"""
+# from cs4660.tutorial import lists
 
+import re
 class SimpleFile(object):
     """SimpleFile tests using file read api to do some simple math"""
     def __init__(self, file_path):
@@ -8,31 +10,41 @@ class SimpleFile(object):
         TODO: reads the file by path and parse content into two
         dimension array (numbers)
         """
+        with open(file_path) as fp:
+            lines = fp.read().split("\n")
+            for line in lines:
+                if len(line) > 0:
+                    parts = list(map(int, line.split(' ')))
+                    # print(parts)
+                    self.numbers.append(parts)
+
+
 
     def get_mean(self, line_number):
         """
         get_mean retrieves the mean value of the list by line_number (starts
         with zero)
         """
-        pass
+        # return lists.get_avg(self.get_max())
+        return sum(self.numbers[line_number])/float(len(self.numbers[line_number]))
 
     def get_max(self, line_number):
         """
         get_max retrieves the maximum value of the list by line_number (starts
         with zero)
         """
-        pass
+        return max(self.numbers[line_number])
 
     def get_min(self, line_number):
         """
         get_min retrieves the minimum value of the list by line_number (starts
         with zero)
         """
-        pass
+        return min(self.numbers[line_number])
 
     def get_sum(self, line_number):
         """
         get_sum retrieves the sumation of the list by line_number (starts with
         zero)
         """
-        pass
+        return sum(self.numbers[line_number])
